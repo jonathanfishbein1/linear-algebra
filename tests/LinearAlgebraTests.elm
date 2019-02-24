e7c33cb0fe3e4234c73ce326b43c54cc51f8036e
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-02-24 18:02:31 -0500

    Verify Vector add is commutative

--- tests/LinearAlgebraTests.elm
+++ tests/LinearAlgebraTests.elm
@@ -9,15 +9,15 @@ import Test
 suite : Test.Test
 suite =
     Test.describe "The LinearAlgebra module"
-        [ Test.fuzz2 Fuzz.int Fuzz.int "tests ComplexNumbers add" <|
+        [ Test.fuzz2 Fuzz.int Fuzz.int "tests Vector add is commutative" <|
             \one two ->
                 let
-                    testValue =
+                    v =
                         LinearAlgebra.Vector [ one, two ]
 
-                    expected =
-                        LinearAlgebra.Vector [ 2 * one, 2 * two ]
+                    w =
+                        LinearAlgebra.Vector [ one, two ]
                 in
-                LinearAlgebra.add (+) testValue testValue
-                    |> Expect.equal expected
+                LinearAlgebra.add (+) v w
+                    |> Expect.equal (LinearAlgebra.add (+) w v)
         ]

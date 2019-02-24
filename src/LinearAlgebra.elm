24ca560aff0e446b0be76bd73b454e24dfe0c232
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-02-24 17:58:27 -0500

    Add addFunction parameter

--- src/LinearAlgebra.elm
+++ src/LinearAlgebra.elm
@@ -8,6 +8,6 @@ type Vector a
     = Vector (List a)
 
 
-add : Vector number -> Vector number -> Vector number
-add (Vector vectorOne) (Vector vectorTwo) =
-    Vector <| List.map2 (+) vectorOne vectorTwo
+add : (a -> a -> a) -> Vector a -> Vector a -> Vector a
+add addFunction (Vector vectorOne) (Vector vectorTwo) =
+    Vector <| List.map2 addFunction vectorOne vectorTwo

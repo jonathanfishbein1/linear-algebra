5bbee22e7222099ab96f1a7fd6521c34160d3b3a
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-02-24 17:50:35 -0500

    Add add function

--- src/LinearAlgebra.elm
+++ src/LinearAlgebra.elm
@@ -3,3 +3,8 @@ module LinearAlgebra exposing (Vector2(..))
 
 type Vector a
     = Vector (List a)
+
+
+add : Vector a -> Vector a -> Vector a
+add vectorOne vectorTwo =
+    List.map2 (+) vectorOne vectorTwo

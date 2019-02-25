c2899453bd7bf4f4027797d1e146a0562ef7e5f6
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-02-24 21:04:41 -0500

    Add map

--- src/LinearAlgebra.elm
+++ src/LinearAlgebra.elm
@@ -17,3 +17,8 @@ zero n a =
 add : (a -> a -> a) -> Vector a -> Vector a -> Vector a
 add addFunction (Vector vectorOne) (Vector vectorTwo) =
     Vector <| List.map2 addFunction vectorOne vectorTwo
+
+
+map : (a -> b) -> Vector a -> Vector b
+map f (Vector vector) =
+    Vector <| List.map f vector

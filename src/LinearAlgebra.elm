596224ab2e2864cde70fc2ff127c292bb1ac88ef
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-02-24 20:47:32 -0500

    Add zero

--- src/LinearAlgebra.elm
+++ src/LinearAlgebra.elm
@@ -8,6 +8,11 @@ type Vector a
     = Vector (List a)
 
 
+zero : Int -> a -> Vector a
+zero n a =
+    Vector <| List.repeat n a
+
+
 add : (a -> a -> a) -> Vector a -> Vector a -> Vector a
 add addFunction (Vector vectorOne) (Vector vectorTwo) =
     Vector <| List.map2 addFunction vectorOne vectorTwo

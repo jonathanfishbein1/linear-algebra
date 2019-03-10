c2e251e26542dee6d10e5ce75f5efb2782c74896
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-03-10 16:51:10 -0400

    Add documentation for Matrix

--- src/Matrix.elm
+++ src/Matrix.elm
@@ -1,17 +1,39 @@
 module Matrix exposing
     ( Matrix(..)
-    , addComplexMatrices
     , addRealMatrices
-    , adjoint
-    , conjugate
-    , equal
+    , addComplexMatrices
+    , sumRealMatrices
+    , sumComplexMatrices
     , map
+    , equal
+    , transpose
+    , conjugate
     , multiplyComplexMatrices
     , multiplyRealMatrices
-    , sumComplexMatrices
-    , sumRealMatrices
-    , transpose
+    , adjoint
     )
+
+{-| A module for Matrix
+
+
+# Types
+
+@docs Matrix
+
+@docs addRealMatrices
+@docs addComplexMatrices
+@docs sumRealMatrices
+@docs sumComplexMatrices
+@docs map
+@docs equal
+@docs transpose
+@docs conjugate
+@docs apply
+@docs liftA2
+@docs multiplyComplexMatrices
+@docs multiplyRealMatrices
+
+-}
 
 import ComplexNumbers
 import List.Extra
@@ -23,11 +45,15 @@ type Matrix a
     = Matrix (List (Vector.Vector a))
 
 
+{-| Add two Real Matrices together
+-}
 addRealMatrices : Matrix number -> Matrix number -> Matrix number
 addRealMatrices =
     liftA2 (+)
 
 
+{-| Add two Complex Matrices together
+-}
 addComplexMatrices : Matrix (ComplexNumbers.ComplexNumberCartesian number) -> Matrix (ComplexNumbers.ComplexNumberCartesian number) -> Matrix (ComplexNumbers.ComplexNumberCartesian number)
 addComplexMatrices =
     liftA2 ComplexNumbers.add
@@ -47,16 +73,22 @@ sumComplexMatrices sumEmptyMatrix =
     Monoid.monoid sumEmptyMatrix addComplexMatrices
 
 
+{-| Map over a Matrix
+-}
 map : (a -> b) -> Matrix a -> Matrix b
 map f (Matrix matrix) =
     Matrix <| List.map (Vector.map f) matrix
 
 
+{-| Compare two Matrices for equality
+-}
 equal : (a -> a -> Bool) -> Matrix a -> Matrix a -> Bool
 equal comparator (Matrix matrixOne) (Matrix matrixTwo) =
     List.all ((==) True) <| List.map2 (Vector.equal comparator) matrixOne matrixTwo
 
 
+{-| Transpose a Matrix
+-}
 transpose : Matrix a -> Matrix a
 transpose (Matrix matrix) =
     matrix
@@ -66,12 +98,16 @@ transpose (Matrix matrix) =
         |> Matrix
 
 
+{-| Take the complex conjugate of a Complex Numbered Matrix
+-}
 conjugate : Matrix (ComplexNumbers.ComplexNumberCartesian number) -> Matrix (ComplexNumbers.ComplexNumberCartesian number)
 conjugate matrix =
     matrix
         |> map ComplexNumbers.conjugate
 
 
+{-| Perform the adjoint operation on a Complex Numbered Matrix
+-}
 adjoint : Matrix (ComplexNumbers.ComplexNumberCartesian number) -> Matrix (ComplexNumbers.ComplexNumberCartesian number)
 adjoint matrix =
     matrix
@@ -79,21 +115,29 @@ adjoint matrix =
         |> transpose
 
 
+{-| Apply for Matrix
+-}
 apply : Matrix (a -> b) -> Matrix a -> Matrix b
 apply (Matrix fMatrix) (Matrix matrix) =
     Matrix <| List.map2 (\fVector xVector -> Vector.apply fVector xVector) fMatrix matrix
 
 
+{-| Lift a function to work on Matrix
+-}
 liftA2 : (a -> b -> c) -> Matrix a -> Matrix b -> Matrix c
 liftA2 f a b =
     apply (map f a) b
 
 
+{-| Matrix Matrix multiplication for a Complex Numbered Matrix
+-}
 multiplyComplexMatrices : Matrix (Vector.Vector (ComplexNumbers.ComplexNumberCartesian number)) -> Matrix (Vector.Vector (ComplexNumbers.ComplexNumberCartesian number)) -> Matrix (Vector.Vector (ComplexNumbers.ComplexNumberCartesian number))
 multiplyComplexMatrices matrixOne matrixTwo =
     liftA2 Vector.multiplyComplexVectors matrixOne (transpose matrixTwo)
 
 
+{-| Matrix Matrix multiplication for a Real Numbered Matrix
+-}
 multiplyRealMatrices : Matrix number -> Matrix number -> Matrix number
 multiplyRealMatrices matrixOne matrixTwo =
     liftA2 (*) matrixOne (transpose matrixTwo)

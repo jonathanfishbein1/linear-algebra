eacc8c2bb79f3f7839966df058107d5fefc56b78
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-03-11 21:14:20 -0400

    Convert fourth parameter to Matrix

--- src/Matrix.elm
+++ src/Matrix.elm
@@ -146,7 +146,7 @@ multiplyComplexMatrices matrixOne matrixTwo =
 -}
 multiplyRealMatrices : Matrix number -> Matrix number -> Matrix number
 multiplyRealMatrices matrixOne matrixTwo =
-    Matrix <| smartMapMatrix2 matrixOne (transpose matrixTwo) (transpose matrixTwo) [] []
+    Matrix <| smartMapMatrix2 matrixOne (transpose matrixTwo) (transpose matrixTwo) [] (Matrix [])
 
 
 diagonal : Int -> Int -> number
@@ -180,14 +180,14 @@ addSumVectors (Vector.Vector vectorOne) (Vector.Vector vectorTwo) =
         |> List.sum
 
 
-smartMapMatrix2 : Matrix number -> Matrix number -> Matrix number -> List number -> List (Vector.Vector number) -> List (Vector.Vector number)
-smartMapMatrix2 (Matrix left) (Matrix right) (Matrix currentRight) intermediateList acc =
+smartMapMatrix2 : Matrix number -> Matrix number -> Matrix number -> List number -> Matrix number -> List (Vector.Vector number)
+smartMapMatrix2 (Matrix left) (Matrix right) (Matrix currentRight) intermediateList (Matrix acc) =
     case ( left, currentRight ) of
         ( l :: ls, r :: rs ) ->
-            smartMapMatrix2 (Matrix left) (Matrix right) (Matrix rs) (intermediateList ++ [ addSumVectors l r ]) acc
+            smartMapMatrix2 (Matrix left) (Matrix right) (Matrix rs) (intermediateList ++ [ addSumVectors l r ]) (Matrix acc)
 
         ( (Vector.Vector l) :: ls, [] ) ->
-            smartMapMatrix2 (Matrix ls) (Matrix right) (Matrix right) [] (acc ++ [ Vector.Vector intermediateList ])
+            smartMapMatrix2 (Matrix ls) (Matrix right) (Matrix right) [] (Matrix (acc ++ [ Vector.Vector intermediateList ]))
 
         ( [], _ ) ->
             acc

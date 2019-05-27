7d4ea645d09a417c1578dec8d1f8fe12b0d824f0
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-05-27 19:32:37 -0400

    Add test for second applicative law for Vector

--- tests/VectorApplicativeFunctorTests.elm
+++ tests/VectorApplicativeFunctorTests.elm
@@ -24,6 +24,31 @@ suite =
                         Vector.apply vIdentity v
                 in
                 Expect.equal vApplied v
+        , Test.fuzz3 Fuzz.int Fuzz.int Fuzz.int "tests second applicative law for Vector" <|
+            \one two three ->
+                let
+                    f =
+                        (<<)
+
+                    fPure =
+                        Vector.pure f
+
+                    u =
+                        Vector.Vector [ identity ]
+
+                    v =
+                        Vector.Vector [ identity ]
+
+                    w =
+                        Vector.Vector [ 0 ]
+
+                    leftSide =
+                        Vector.apply (Vector.apply (Vector.apply fPure u) v) w
+
+                    rightSide =
+                        Vector.apply u (Vector.apply v w)
+                in
+                Expect.equal leftSide rightSide
         , Test.fuzz Fuzz.int "tests third applicative law for Vector" <|
             \one ->
                 let

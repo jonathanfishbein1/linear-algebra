54597225bb6d1e79ad2faf9ad550b252ec7da70e
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-09-16 18:51:05 -0400

    FInish MatrixInnerProductSpaceTests

--- tests/MatrixTests/MatrixInnerProductSpaceTests.elm
+++ tests/MatrixTests/MatrixInnerProductSpaceTests.elm
@@ -33,25 +33,25 @@ suite =
             \one two three ->
                 let
                     a =
-                        Vector.Vector [ one ]
+                        Matrix.Matrix <| [ Matrix.RowVector <| Vector.Vector [ one ] ]
 
                     b =
-                        Vector.Vector [ two ]
+                        Matrix.Matrix <| [ Matrix.RowVector <| Vector.Vector [ two ] ]
 
                     c =
-                        Vector.Vector [ three ]
+                        Matrix.Matrix <| [ Matrix.RowVector <| Vector.Vector [ three ] ]
 
                     aPlusBDotc =
-                        Vector.vectorDotProduct Field.realField (Vector.addVectors Field.realField a b) c
+                        Matrix.dotProduct Vector.realInnerProductSpace (Matrix.addMatrices Field.realField a b) c
 
                     aDotB =
-                        Vector.vectorDotProduct Field.realField a c
+                        Matrix.dotProduct Vector.realInnerProductSpace a c
 
                     bDotC =
-                        Vector.vectorDotProduct Field.realField b c
+                        Matrix.dotProduct Vector.realInnerProductSpace b c
 
                     aDotBPlusbDotC =
-                        aDotB + bDotC
+                        Result.map2 (+) aDotB bDotC
                 in
                 aPlusBDotc
                     |> Expect.equal aDotBPlusbDotC
@@ -59,16 +59,16 @@ suite =
             \one two three ->
                 let
                     a =
-                        Vector.Vector [ one ]
+                        Matrix.Matrix <| [ Matrix.RowVector <| Vector.Vector [ one ] ]
 
                     b =
-                        Vector.Vector [ two ]
+                        Matrix.Matrix <| [ Matrix.RowVector <| Vector.Vector [ two ] ]
 
                     threeTimesADotB =
-                        Vector.vectorDotProduct Field.realField (Vector.map ((*) three) a) b
+                        Matrix.dotProduct Vector.realInnerProductSpace (Matrix.map ((*) three) a) b
 
                     aDotBTimesThree =
-                        Vector.vectorDotProduct Field.realField a b * three
+                        Result.map2 (*) (Matrix.dotProduct Vector.realInnerProductSpace a b) (Ok three)
                 in
                 threeTimesADotB
                     |> Expect.equal aDotBTimesThree
@@ -76,124 +76,17 @@ suite =
             \one two ->
                 let
                     a =
-                        Vector.Vector [ one ]
+                        Matrix.Matrix <| [ Matrix.RowVector <| Vector.Vector [ one ] ]
 
                     b =
-                        Vector.Vector [ two ]
+                        Matrix.Matrix <| [ Matrix.RowVector <| Vector.Vector [ two ] ]
 
                     aDotB =
-                        Vector.vectorDotProduct Field.realField a b
+                        Matrix.dotProduct Vector.realInnerProductSpace a b
 
                     bDotA =
-                        Vector.vectorDotProduct Field.realField b a
+                        Matrix.dotProduct Vector.realInnerProductSpace b a
                 in
                 aDotB
                     |> Expect.equal bDotA
-        , Test.fuzz (Fuzz.map toFloat (Fuzz.intRange -10 10)) "tests vector length equals square of dot product" <|
-            \one ->
-                let
-                    a =
-                        Vector.Vector [ one ]
-
-                    squareRootADotA =
-                        Basics.sqrt (Vector.vectorDotProduct Field.realField a a)
-
-                    aLength =
-                        Vector.vectorLength Field.realField a
-                in
-                squareRootADotA
-                    |> Expect.equal aLength
-        , Test.fuzz Fuzz.float "tests vector length is nondegenerative" <|
-            \one ->
-                let
-                    a =
-                        Vector.Vector [ one ]
-
-                    expected =
-                        Vector.vectorLength Field.realField a
-                in
-                expected
-                    |> Expect.atLeast 0
-        , Test.fuzz2 (Fuzz.map toFloat (Fuzz.intRange -10 10)) (Fuzz.map toFloat (Fuzz.intRange -10 10)) "tests vector length satisfies triangle inequality" <|
-            \one two ->
-                let
-                    a =
-                        Vector.Vector [ one ]
-
-                    b =
-                        Vector.Vector [ two ]
-
-                    aPlusBLength =
-                        Vector.vectorLength Field.realField <| Vector.addVectors Field.realField a b
-
-                    lengthAPlusLengthB =
-                        Vector.vectorLength Field.realField a + Vector.vectorLength Field.realField b
-                in
-                aPlusBLength
-                    |> Expect.atMost lengthAPlusLengthB
-        , Test.fuzz2 (Fuzz.floatRange -10 10) (Fuzz.floatRange -10 10) "tests vector length respects scalar multiplication" <|
-            \one two ->
-                let
-                    a =
-                        Vector.Vector [ one ]
-
-                    legnthOfTwoTimesA =
-                        Vector.vectorLength Field.realField (Vector.map ((*) two) a)
-
-                    lengthOfATimesTwo =
-                        Basics.abs two * Vector.vectorLength Field.realField a
-                in
-                legnthOfTwoTimesA
-                    |> Expect.within (Expect.Absolute 0.1) lengthOfATimesTwo
-        , Test.fuzz Fuzz.float "tests distance is nondegenerative" <|
-            \one ->
-                let
-                    a =
-                        Vector.Vector [ one ]
-
-                    expected =
-                        Vector.distance Vector.realVectorAbelianGroup a a
-                in
-                expected
-                    |> Expect.atLeast 0
-        , Test.fuzz3 (Fuzz.map toFloat (Fuzz.intRange -10 10)) (Fuzz.map toFloat (Fuzz.intRange -10 10)) (Fuzz.map toFloat (Fuzz.intRange -10 10)) "tests vector distance satisfies triangle inequality" <|
-            \one two three ->
-                let
-                    a =
-                        Vector.Vector [ one ]
-
-                    b =
-                        Vector.Vector [ two ]
-
-                    c =
-                        Vector.Vector [ three ]
-
-                    distanceAB =
-                        Vector.distance Vector.realVectorAbelianGroup a b
-
-                    distanceAC =
-                        Vector.distance Vector.realVectorAbelianGroup a c
-
-                    distanceCB =
-                        Vector.distance Vector.realVectorAbelianGroup c b
-                in
-                distanceAB
-                    |> Expect.atMost (distanceAC + distanceCB)
-        , Test.fuzz2 (Fuzz.map toFloat (Fuzz.intRange -10 10)) (Fuzz.map toFloat (Fuzz.intRange -10 10)) "tests distance is symetric" <|
-            \one two ->
-                let
-                    a =
-                        Vector.Vector [ one ]
-
-                    b =
-                        Vector.Vector [ two ]
-
-                    distanceAB =
-                        Vector.vectorDotProduct Field.realField a b
-
-                    distanceBA =
-                        Vector.vectorDotProduct Field.realField b a
-                in
-                distanceAB
-                    |> Expect.equal distanceBA
         ]

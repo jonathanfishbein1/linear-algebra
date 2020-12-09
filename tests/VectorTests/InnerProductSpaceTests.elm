07d6177354893f2671c4d9ced9e4841de1969173
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2020-12-09 12:54:04 -0500

    Uncomment InnerProductSpaceTests

--- tests/VectorTests/InnerProductSpaceTests.elm
+++ tests/VectorTests/InnerProductSpaceTests.elm
@@ -101,207 +101,230 @@ suite =
                         Vector.dotProduct Field.numberField b a
                 in
                 Expect.within (Expect.Absolute 0.000000001) bDotA aDotB
+        , Test.fuzz
+            (Fuzz.floatRange -10 10)
+            "tests vector length equals square root of dot product"
+          <|
+            \one ->
+                let
+                    a =
+                        Vector.Vector [ one ]
 
-        -- , Test.fuzz
-        --     (Fuzz.floatRange -10 10)
-        --     "tests vector length equals square root of dot product"
-        --   <|
-        --     \one ->
-        --         let
-        --             a =
-        --                 Vector.Vector [ one ]
-        --             squareRootADotA =
-        --                 Basics.sqrt (Vector.dotProduct Field.numberField a a)
-        --             aLength =
-        --                 Vector.length Field.numberField a
-        --         in
-        --         squareRootADotA
-        --             |> Expect.within (Expect.Absolute 0.000000001) aLength
-        -- , Test.fuzz
-        --     Fuzz.float
-        --     "tests vector length is nondegenerative"
-        --   <|
-        --     \one ->
-        --         let
-        --             a =
-        --                 Vector.Vector [ one ]
-        --             expected =
-        --                 Vector.length Field.numberField a
-        --         in
-        --         expected
-        --             |> Expect.atLeast 0
-        -- , Test.fuzz2
-        --     (Fuzz.floatRange -10 10)
-        --     (Fuzz.floatRange -10 10)
-        --     "tests vector dot product satisfies Cauchy-Shwartz inequality"
-        --   <|
-        --     \one two ->
-        --         let
-        --             x =
-        --                 Vector.Vector [ one ]
-        --             y =
-        --                 Vector.Vector [ two ]
-        --             absXDotY =
-        --                 Vector.dotProduct
-        --                     Field.numberField
-        --                     x
-        --                     y
-        --                     |> Basics.abs
-        --             lengthOfX =
-        --                 Vector.length Field.numberField x
-        --             lengthOfY =
-        --                 Vector.length Field.numberField y
-        --             lengthOfXTimesLengthOfY =
-        --                 lengthOfX * lengthOfY
-        --         in
-        --         absXDotY
-        --             |> Expect.atMost lengthOfXTimesLengthOfY
-        -- , Test.fuzz2
-        --     (Fuzz.floatRange -10 10)
-        --     (Fuzz.floatRange -10 10)
-        --     "tests vector length satisfies triangle inequality"
-        --   <|
-        --     \one two ->
-        --         let
-        --             a =
-        --                 Vector.Vector [ one ]
-        --             b =
-        --                 Vector.Vector [ two ]
-        --             aPlusBLength =
-        --                 Vector.addVectors
-        --                     Field.numberField
-        --                     a
-        --                     b
-        --                     |> Vector.length
-        --                         Field.numberField
-        --             lengthAPlusLengthB =
-        --                 Vector.length
-        --                     Field.numberField
-        --                     a
-        --                     + Vector.length
-        --                         Field.numberField
-        --                         b
-        --         in
-        --         aPlusBLength
-        --             |> Expect.atMost lengthAPlusLengthB
-        -- , Test.fuzz2
-        --     (Fuzz.floatRange -10 10)
-        --     (Fuzz.floatRange -10 10)
-        --     "tests vector length respects scalar multiplication"
-        --   <|
-        --     \one two ->
-        --         let
-        --             a =
-        --                 Vector.Vector [ one ]
-        --             legnthOfTwoTimesA =
-        --                 Vector.length
-        --                     Field.numberField
-        --                     (Vector.scalarMultiplication Field.numberField two a)
-        --             lengthOfATimesTwo =
-        --                 Basics.abs two * Vector.length Field.numberField a
-        --         in
-        --         legnthOfTwoTimesA
-        --             |> Expect.within (Expect.Absolute 0.000000001) lengthOfATimesTwo
-        -- , Test.fuzz
-        --     Fuzz.float
-        --     "tests distance is nondegenerative"
-        --   <|
-        --     \one ->
-        --         let
-        --             a =
-        --                 Vector.Vector [ one ]
-        --             expected =
-        --                 Vector.distance Vector.realVectorAbelianGroup a a
-        --         in
-        --         expected
-        --             |> Expect.atLeast 0
-        -- , Test.fuzz3
-        --     (Fuzz.map toFloat Fuzz.int)
-        --     (Fuzz.map toFloat Fuzz.int)
-        --     (Fuzz.map toFloat Fuzz.int)
-        --     "tests vector distance satisfies triangle inequality"
-        --   <|
-        --     \one two three ->
-        --         let
-        --             a =
-        --                 Vector.Vector [ one ]
-        --             b =
-        --                 Vector.Vector [ two ]
-        --             c =
-        --                 Vector.Vector [ three ]
-        --             distanceAB =
-        --                 Vector.distance Vector.realVectorAbelianGroup a b
-        --             distanceAC =
-        --                 Vector.distance Vector.realVectorAbelianGroup a c
-        --             distanceCB =
-        --                 Vector.distance Vector.realVectorAbelianGroup c b
-        --         in
-        --         distanceAB
-        --             |> Expect.atMost (distanceAC + distanceCB)
-        -- , Test.fuzz2
-        --     (Fuzz.floatRange -10 10)
-        --     (Fuzz.floatRange -10 10)
-        --     "tests distance is symetric"
-        --   <|
-        --     \one two ->
-        --         let
-        --             a =
-        --                 Vector.Vector [ one ]
-        --             b =
-        --                 Vector.Vector [ two ]
-        --             distanceAB =
-        --                 Vector.dotProduct
-        --                     Field.numberField
-        --                     a
-        --                     b
-        --             distanceBA =
-        --                 Vector.dotProduct
-        --                     Field.numberField
-        --                     b
-        --                     a
-        --         in
-        --         distanceAB
-        --             |> Expect.within (Expect.Absolute 0.000000001) distanceBA
-        -- , Test.test
-        --     "tests angle between orthagonal vectors"
-        --   <|
-        --     \_ ->
-        --         let
-        --             a =
-        --                 Vector.Vector [ 1, 0 ]
-        --             b =
-        --                 Vector.Vector [ 0, 1 ]
-        --             angle =
-        --                 Vector.angleBetween a b
-        --         in
-        --         angle
-        --             |> Expect.within (Expect.Absolute 0.000000001) (Basics.pi / 2)
-        -- , Test.test
-        --     "tests angle between colinear vectors"
-        --   <|
-        --     \_ ->
-        --         let
-        --             a =
-        --                 Vector.Vector [ 1, 0 ]
-        --             b =
-        --                 Vector.Vector [ 1, 0 ]
-        --             angle =
-        --                 Vector.angleBetween a b
-        --         in
-        --         angle
-        --             |> Expect.within (Expect.Absolute 0.000000001) 0
-        -- , Test.test
-        --     "tests angle between colinear but opposite vectors"
-        --   <|
-        --     \_ ->
-        --         let
-        --             a =
-        --                 Vector.Vector [ 1, 0 ]
-        --             b =
-        --                 Vector.Vector [ -1, 0 ]
-        --             angle =
-        --                 Vector.angleBetween a b
-        --         in
-        --         angle
-        --             |> Expect.within (Expect.Absolute 0.000000001) Basics.pi
+                    squareRootADotA =
+                        Basics.sqrt (Vector.dotProduct Field.numberField a a)
+
+                    aLength =
+                        Vector.lengthReal a
+                in
+                squareRootADotA
+                    |> Expect.within (Expect.Absolute 0.000000001) aLength
+        , Test.fuzz
+            Fuzz.float
+            "tests vector length is nondegenerative"
+          <|
+            \one ->
+                let
+                    a =
+                        Vector.Vector [ one ]
+
+                    expected =
+                        Vector.lengthReal a
+                in
+                expected
+                    |> Expect.atLeast 0
+        , Test.fuzz2
+            (Fuzz.floatRange -10 10)
+            (Fuzz.floatRange -10 10)
+            "tests vector dot product satisfies Cauchy-Shwartz inequality"
+          <|
+            \one two ->
+                let
+                    x =
+                        Vector.Vector [ one ]
+
+                    y =
+                        Vector.Vector [ two ]
+
+                    absXDotY =
+                        Vector.dotProduct
+                            Field.numberField
+                            x
+                            y
+                            |> Basics.abs
+
+                    lengthOfX =
+                        Vector.lengthReal x
+
+                    lengthOfY =
+                        Vector.lengthReal y
+
+                    lengthOfXTimesLengthOfY =
+                        lengthOfX * lengthOfY
+                in
+                absXDotY
+                    |> Expect.atMost lengthOfXTimesLengthOfY
+        , Test.fuzz2
+            (Fuzz.floatRange -10 10)
+            (Fuzz.floatRange -10 10)
+            "tests vector length satisfies triangle inequality"
+          <|
+            \one two ->
+                let
+                    a =
+                        Vector.Vector [ one ]
+
+                    b =
+                        Vector.Vector [ two ]
+
+                    aPlusBLength =
+                        Vector.addVectors
+                            Field.numberField
+                            a
+                            b
+                            |> Vector.lengthReal
+
+                    lengthAPlusLengthB =
+                        Vector.lengthReal
+                            a
+                            + Vector.lengthReal
+                                b
+                in
+                aPlusBLength
+                    |> Expect.atMost lengthAPlusLengthB
+        , Test.fuzz2
+            (Fuzz.floatRange -10 10)
+            (Fuzz.floatRange -10 10)
+            "tests vector length respects scalar multiplication"
+          <|
+            \one two ->
+                let
+                    a =
+                        Vector.Vector [ one ]
+
+                    legnthOfTwoTimesA =
+                        Vector.lengthReal
+                            (Vector.scalarMultiplication Field.numberField two a)
+
+                    lengthOfATimesTwo =
+                        Basics.abs two * Vector.lengthReal a
+                in
+                legnthOfTwoTimesA
+                    |> Expect.within (Expect.Absolute 0.000000001) lengthOfATimesTwo
+        , Test.fuzz
+            Fuzz.float
+            "tests distance is nondegenerative"
+          <|
+            \one ->
+                let
+                    a =
+                        Vector.Vector [ one ]
+
+                    expected =
+                        Vector.distance Vector.realInnerProductSpace a a
+                in
+                expected
+                    |> Expect.atLeast 0
+        , Test.fuzz3
+            (Fuzz.map toFloat Fuzz.int)
+            (Fuzz.map toFloat Fuzz.int)
+            (Fuzz.map toFloat Fuzz.int)
+            "tests vector distance satisfies triangle inequality"
+          <|
+            \one two three ->
+                let
+                    a =
+                        Vector.Vector [ one ]
+
+                    b =
+                        Vector.Vector [ two ]
+
+                    c =
+                        Vector.Vector [ three ]
+
+                    distanceAB =
+                        Vector.distance Vector.realInnerProductSpace a b
+
+                    distanceAC =
+                        Vector.distance Vector.realInnerProductSpace a c
+
+                    distanceCB =
+                        Vector.distance Vector.realInnerProductSpace c b
+                in
+                distanceAB
+                    |> Expect.atMost (distanceAC + distanceCB)
+        , Test.fuzz2
+            (Fuzz.floatRange -10 10)
+            (Fuzz.floatRange -10 10)
+            "tests distance is symetric"
+          <|
+            \one two ->
+                let
+                    a =
+                        Vector.Vector [ one ]
+
+                    b =
+                        Vector.Vector [ two ]
+
+                    distanceAB =
+                        Vector.dotProduct
+                            Field.numberField
+                            a
+                            b
+
+                    distanceBA =
+                        Vector.dotProduct
+                            Field.numberField
+                            b
+                            a
+                in
+                distanceAB
+                    |> Expect.within (Expect.Absolute 0.000000001) distanceBA
+        , Test.test
+            "tests angle between orthagonal vectors"
+          <|
+            \_ ->
+                let
+                    a =
+                        Vector.Vector [ 1, 0 ]
+
+                    b =
+                        Vector.Vector [ 0, 1 ]
+
+                    angle =
+                        Vector.angleBetween a b
+                in
+                angle
+                    |> Expect.within (Expect.Absolute 0.000000001) (Basics.pi / 2)
+        , Test.test
+            "tests angle between colinear vectors"
+          <|
+            \_ ->
+                let
+                    a =
+                        Vector.Vector [ 1, 0 ]
+
+                    b =
+                        Vector.Vector [ 1, 0 ]
+
+                    angle =
+                        Vector.angleBetween a b
+                in
+                angle
+                    |> Expect.within (Expect.Absolute 0.000000001) 0
+        , Test.test
+            "tests angle between colinear but opposite vectors"
+          <|
+            \_ ->
+                let
+                    a =
+                        Vector.Vector [ 1, 0 ]
+
+                    b =
+                        Vector.Vector [ -1, 0 ]
+
+                    angle =
+                        Vector.angleBetween a b
+                in
+                angle
+                    |> Expect.within (Expect.Absolute 0.000000001) Basics.pi
         ]

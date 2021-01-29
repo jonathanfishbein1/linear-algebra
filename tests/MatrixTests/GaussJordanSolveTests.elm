3bbcb5a6eb28e4e812d36019bc764b7c123d522a
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2021-01-29 12:04:51 -0500

    Add GaussJordanSolveTests

---
+++ tests/MatrixTests/GaussJordanSolveTests.elm
@@ -0,0 +1,141 @@
+module MatrixTests.GaussJordanSolveTests exposing (suite)
+
+import ComplexNumbers
+import Expect
+import Float.Extra
+import Matrix
+import Test
+import Vector
+
+
+suite : Test.Test
+suite =
+    Test.describe "The LinearAlgebra module"
+        [ Test.test
+            "tests matrix jordanReduce put matrix into Reduced Row Echelon Form"
+          <|
+            \_ ->
+                let
+                    matrix =
+                        Matrix.Matrix
+                            [ Matrix.RowVector <| Vector.Vector [ 1, 2, -1, -4 ]
+                            , Matrix.RowVector <| Vector.Vector [ 2, 3, -1, -11 ]
+                            , Matrix.RowVector <| Vector.Vector [ -2, 0, -3, 22 ]
+                            ]
+
+                    reducedRowEchelonFormMatrix =
+                        Matrix.gaussJordan Vector.realVectorSpace matrix
+
+                    expected =
+                        Matrix.Matrix <|
+                            [ Matrix.RowVector <| Vector.Vector [ 1.0, 0.0, 0.0, -8.0 ]
+                            , Matrix.RowVector <| Vector.Vector [ 0.0, 1.0, 0.0, 1.0 ]
+                            , Matrix.RowVector <| Vector.Vector [ 0.0, 0.0, 1.0, -2.0 ]
+                            ]
+                in
+                Expect.equal reducedRowEchelonFormMatrix expected
+        , Test.test
+            "tests matrix gaussJordan produces correct answers"
+          <|
+            \_ ->
+                let
+                    matrix =
+                        Matrix.Matrix
+                            [ Matrix.RowVector <| Vector.Vector [ 1, 2, -1 ]
+                            , Matrix.RowVector <| Vector.Vector [ 2, 3, -1 ]
+                            , Matrix.RowVector <| Vector.Vector [ -2, 0, -3 ]
+                            ]
+
+                    b =
+                        Matrix.ColumnVector <| Vector.Vector [ -4, -11, 22 ]
+
+                    reducedRowEchelonFormMatrix =
+                        Matrix.solve Vector.realInnerProductSpace matrix b
+
+                    expected =
+                        Matrix.ColumnVector <| Vector.Vector [ -8.0, 1.0, -2.0 ]
+                in
+                Expect.equal reducedRowEchelonFormMatrix (Matrix.Consistant (Matrix.UniqueSolution expected))
+        , Test.test
+            "tests matrix gaussJordan produces correct answers second example"
+          <|
+            \_ ->
+                let
+                    matrix =
+                        Matrix.Matrix
+                            [ Matrix.RowVector <| Vector.Vector [ 1, 1, 1 ]
+                            , Matrix.RowVector <| Vector.Vector [ 1, 2, 3 ]
+                            , Matrix.RowVector <| Vector.Vector [ 1, 3, 4 ]
+                            ]
+
+                    b =
+                        Matrix.ColumnVector <| Vector.Vector [ 3, 0, -2 ]
+
+                    reducedRowEchelonFormMatrix =
+                        Matrix.solve Vector.realInnerProductSpace matrix b
+
+                    expected =
+                        Matrix.ColumnVector <| Vector.Vector [ 5, -1.0, -1.0 ]
+                in
+                Expect.equal reducedRowEchelonFormMatrix (Matrix.Consistant (Matrix.UniqueSolution expected))
+        , Test.test
+            "tests matrix gaussJordan with no solutions"
+          <|
+            \_ ->
+                let
+                    matrix =
+                        Matrix.Matrix
+                            [ Matrix.RowVector <| Vector.Vector [ 1, 2, 1, 1 ]
+                            , Matrix.RowVector <| Vector.Vector [ 1, 2, 2, -1 ]
+                            , Matrix.RowVector <| Vector.Vector [ 2, 4, 0, 6 ]
+                            ]
+
+                    b =
+                        Matrix.ColumnVector <| Vector.Vector [ 8, 12, 4 ]
+
+                    reducedRowEchelonFormMatrix =
+                        Matrix.solve Vector.realInnerProductSpace matrix b
+                in
+                Expect.equal reducedRowEchelonFormMatrix (Matrix.Inconsistant "No Unique Solution")
+        , Test.test
+            "tests matrix gaussJordan with infinite solutions"
+          <|
+            \_ ->
+                let
+                    matrix =
+                        Matrix.Matrix
+                            [ Matrix.RowVector <| Vector.Vector [ 1, 2, 1, 1 ]
+                            , Matrix.RowVector <| Vector.Vector [ 1, 2, 2, -1 ]
+                            , Matrix.RowVector <| Vector.Vector [ 2, 4, 0, 6 ]
+                            ]
+
+                    b =
+                        Matrix.ColumnVector <| Vector.Vector [ 7, 12, 4 ]
+
+                    reducedRowEchelonFormMatrix =
+                        Matrix.solve Vector.realInnerProductSpace matrix b
+                in
+                Expect.equal reducedRowEchelonFormMatrix
+                    (Matrix.Consistant (Matrix.InfiniteSolutions { nullity = 3, rank = 2 }))
+        , Test.test
+            "tests matrix solve, github issue 2"
+          <|
+            \_ ->
+                let
+                    matrix =
+                        Matrix.Matrix
+                            [ Matrix.RowVector (Vector.Vector [ 0, 1, 0, 0 ])
+                            , Matrix.RowVector (Vector.Vector [ 0, 0, 0, 1 ])
+                            , Matrix.RowVector (Vector.Vector [ 0, 0, -2, 1 ])
+                            , Matrix.RowVector (Vector.Vector [ -2, 1, 0, 0 ])
+                            ]
+
+                    b =
+                        Matrix.ColumnVector <| Vector.Vector [ 312, 184, 0, 0 ]
+
+                    reducedRowEchelonFormMatrix =
+                        Matrix.solve Vector.realInnerProductSpace matrix b
+                in
+                Expect.equal reducedRowEchelonFormMatrix
+                    (Matrix.Consistant (Matrix.UniqueSolution (Matrix.ColumnVector (Vector.Vector [ 156, 312, 92, 184 ]))))
+        ]
--- tests/MatrixTests/SquareMatrixTests.elm
+++ tests/MatrixTests/SquareMatrixTests.elm
@@ -2,6 +2,8 @@ module MatrixTests.SquareMatrixTests exposing (suite)
 
 import DoublyStochasticMatrix exposing (DoublyStochasticMatrix(..))
 import Expect
+import Field
+import Fuzz
 import Matrix
 import SquareMatrix
 import Test
@@ -45,4 +47,207 @@ suite =
                         SquareMatrix.isLeftStochastic matrix
                 in
                 Expect.true "Is Left Stochastic" isLeftStochastic
+        , Test.fuzz
+            (Fuzz.map Basics.toFloat Fuzz.int)
+            "tests dot product is nondegenerative"
+          <|
+            \one ->
+                let
+                    a =
+                        [ Matrix.RowVector <| Vector.Vector [ one ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    expected =
+                        SquareMatrix.dotProduct Vector.realInnerProductSpace a a
+                in
+                case expected of
+                    Ok exp ->
+                        exp
+                            |> Expect.atLeast 0
+
+                    Err err ->
+                        Expect.fail err
+        , Test.fuzz3
+            (Fuzz.map toFloat (Fuzz.intRange -10 10))
+            (Fuzz.map toFloat (Fuzz.intRange -10 10))
+            (Fuzz.map toFloat (Fuzz.intRange -10 10))
+            "tests dot product respects addition"
+          <|
+            \one two three ->
+                let
+                    a =
+                        [ Matrix.RowVector <| Vector.Vector [ one ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    b =
+                        [ Matrix.RowVector <| Vector.Vector [ two ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    c =
+                        [ Matrix.RowVector <| Vector.Vector [ three ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    aPlusBDotc =
+                        SquareMatrix.dotProduct Vector.realInnerProductSpace (SquareMatrix.add Field.float a b) c
+
+                    aDotB =
+                        SquareMatrix.dotProduct Vector.realInnerProductSpace a c
+
+                    bDotC =
+                        SquareMatrix.dotProduct Vector.realInnerProductSpace b c
+
+                    aDotBPlusbDotC =
+                        Result.map2
+                            (+)
+                            aDotB
+                            bDotC
+                in
+                aPlusBDotc
+                    |> Expect.equal aDotBPlusbDotC
+        , Test.fuzz3
+            (Fuzz.map toFloat (Fuzz.intRange -10 10))
+            (Fuzz.map toFloat (Fuzz.intRange -10 10))
+            (Fuzz.map toFloat (Fuzz.intRange -10 10))
+            "tests dot product respects scalar multiplication"
+          <|
+            \one two three ->
+                let
+                    a =
+                        [ Matrix.RowVector <| Vector.Vector [ one ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    b =
+                        [ Matrix.RowVector <| Vector.Vector [ two ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    threeTimesADotB =
+                        SquareMatrix.dotProduct Vector.realInnerProductSpace (SquareMatrix.scalarMultiplication Field.float three a) b
+
+                    aDotBTimesThree =
+                        Result.map2
+                            (*)
+                            (SquareMatrix.dotProduct Vector.realInnerProductSpace a b)
+                            (Ok three)
+                in
+                threeTimesADotB
+                    |> Expect.equal aDotBTimesThree
+        , Test.fuzz2
+            (Fuzz.map toFloat (Fuzz.intRange -10 10))
+            (Fuzz.map toFloat (Fuzz.intRange -10 10))
+            "tests dot product is symetric"
+          <|
+            \one two ->
+                let
+                    a =
+                        [ Matrix.RowVector <| Vector.Vector [ one ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    b =
+                        [ Matrix.RowVector <| Vector.Vector [ two ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    aDotB =
+                        SquareMatrix.dotProduct Vector.realInnerProductSpace a b
+
+                    bDotA =
+                        SquareMatrix.dotProduct Vector.realInnerProductSpace b a
+                in
+                aDotB
+                    |> Expect.equal bDotA
+        , Test.fuzz
+            Fuzz.float
+            "tests matrix norm is nondegenerative"
+          <|
+            \one ->
+                let
+                    a =
+                        [ Matrix.RowVector <| Vector.Vector [ one ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    expected =
+                        SquareMatrix.normReal a
+                in
+                case expected of
+                    Ok norm ->
+                        Expect.atLeast 0 norm
+
+                    Err err ->
+                        Expect.fail err
+        , Test.fuzz2
+            (Fuzz.map toFloat (Fuzz.intRange -10 10))
+            (Fuzz.map toFloat (Fuzz.intRange -10 10))
+            "tests matrix norm satisfies triangle inequality"
+          <|
+            \one two ->
+                let
+                    a =
+                        [ Matrix.RowVector <| Vector.Vector [ one ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    b =
+                        [ Matrix.RowVector <| Vector.Vector [ two ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    aPlusBLength =
+                        SquareMatrix.normReal (SquareMatrix.add Field.float a b)
+
+                    lengthAPlusLengthB =
+                        Result.map2
+                            (+)
+                            (SquareMatrix.normReal a)
+                            (SquareMatrix.normReal b)
+                in
+                case aPlusBLength of
+                    Ok aBLength ->
+                        case lengthAPlusLengthB of
+                            Ok otherLength ->
+                                Expect.atMost otherLength aBLength
+
+                            Err err ->
+                                Expect.fail err
+
+                    Err err ->
+                        Expect.fail err
+        , Test.fuzz2
+            Fuzz.float
+            Fuzz.float
+            "tests matrix norm respects scalar multiplication"
+          <|
+            \one two ->
+                let
+                    a =
+                        [ Matrix.RowVector <| Vector.Vector [ one ] ]
+                            |> Matrix.Matrix
+                            |> SquareMatrix.SquareMatrix
+
+                    legnthOfTwoTimesA =
+                        SquareMatrix.normReal (SquareMatrix.scalarMultiplication Field.float two a)
+
+                    lengthOfATimesTwo =
+                        Result.map
+                            ((*) two >> Basics.abs)
+                            (SquareMatrix.normReal a)
+                in
+                case legnthOfTwoTimesA of
+                    Ok twoAL ->
+                        case lengthOfATimesTwo of
+                            Ok lTimesTwo ->
+                                Expect.within (Expect.Absolute 0.1) twoAL lTimesTwo
+
+                            Err err ->
+                                Expect.fail err
+
+                    Err err ->
+                        Expect.fail err
         ]
